4b0b836ea4fcc12cc8d9159cb29fc7de7f5423ae
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2022-10-10 16:07:35 +0545

    Added serilization and deserialization of data

--- websocket.py
+++ websocket.py
@@ -4,34 +4,45 @@ from flask_cors import CORS
 
 from predictnextconfig import predictNextConfiguration
 
+import json
+
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+#socketio stuffs
 socketio = SocketIO(app)
+yolo_event = "YOLO_EVENT"
+light_change_needed_event = "LIGHT_CHANGE_NEEDED_EVENT"
+
+# whether to enable debugging
+debug = False
 
 @app.route("/")
 def main():
     socketio.emit("start event","main method called")
     return "Start of api call"
 
-@socketio.on('traffic')
-def trafficControl(data):
-    print(data)
-
 # for receiving data from client and sending back the output
 # this one is particularly for the Machine Learning model input
 # the input is an array and the output is an integer
-@socketio.event(namespace='/predict-traffic')
-def response(server_id, received_data):
-    next_configuration = predictNextConfiguration(received_data)
 
-    #**************Debug******************
-    print(f"\nserver_id: {server_id}. received_data: {received_data}")
-    print(f"\nPredicted configuration: {next_configuration}")
-    #**************************************
+@socketio.on(light_change_needed_event)
+def get_next_light_configuration(received_data):
+    deserialized_received_data = json.loads(received_data)
 
-    socketio.send(next_configuration)
-    
+    next_configuration = predictNextConfiguration(deserialized_received_data)
+
+    if debug:
+        #**************Debug******************
+        print(f"\nReceived_data: {deserialized_received_data}")
+        print(f"\nPredicted configuration: {next_configuration}")
+        #**************************************
+
+    serialized_next_configuration = json.dumps(next_configuration)
+
+    socketio.emit(light_change_needed_event, serialized_next_configuration)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
